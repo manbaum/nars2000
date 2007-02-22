c9bf2d574aad24e2c340331422d0bfcb1c820899
bsmith <bsmith@5d1e9092-042a-4af3-a4b6-76de6f5dccff>
2007-02-22 02:46:59 +0000

    Added a folder remotely

---
+++ WineHQ/local.h
@@ -0,0 +1,27 @@
+
+#define strlenW 		(UINT) lstrlenW
+#define strcpyW 		lstrcpyW
+
+#define WINAPI			__stdcall
+typedef int 			BOOL,		*PBOOL,    *LPBOOL;
+typedef int 			INT,		*PINT,	   *LPINT;
+#define CHAR			char
+#define UINT			unsigned int
+typedef unsigned long	DWORD,		*PDWORD,   *LPDWORD;
+typedef unsigned short	WCHAR,		*PWCHAR;
+typedef CHAR		   *PSTR,		*LPSTR, 	*NPSTR;
+typedef const CHAR	   *PCSTR,		*LPCSTR;
+typedef WCHAR		   *PWSTR,		*LPWSTR,	*NWPSTR;
+typedef const WCHAR    *PCWSTR, 	*LPCWSTR;
+
+#define CP_ACP			0
+#define USE_IME 		0
+
+INT 		WINAPI MultiByteToWideChar(UINT,DWORD,LPCSTR,INT,LPWSTR,INT);
+INT 		WINAPI WideCharToMultiByte(UINT,DWORD,LPCWSTR,INT,LPSTR,INT,LPCSTR,LPBOOL);
+
+void DbgBrk (void);
+
+#include "editctrl.h"
+#include "resource.h"
+

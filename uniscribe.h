14c48602a16b27081e6bc4177416f9adf35e5fac
bsmith <bsmith@5d1e9092-042a-4af3-a4b6-76de6f5dccff>
2009-05-06 00:01:15 +0000

    Build #407 -- Version 0.0.1.12 ------------------------------
    Directory trunk:
    
    MAKEFILE.W32:
    MAKEFILE.W64:
    PF_DARROW.C:
    QF_SYSID.C:
    UNISCRIBE.H:
      Update copyright year.

--- uniscribe.h
+++ uniscribe.h
@@ -1,10 +1,10 @@
 //***************************************************************************
-//  NARS2000 -- Per Tab Data Header
+//  NARS2000 -- Uniscribe Header
 //***************************************************************************
 
 /***************************************************************************
     NARS2000 -- An Experimental APL Interpreter
-    Copyright (C) 2006-2008 Sudley Place Software
+    Copyright (C) 2006-2009 Sudley Place Software
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by

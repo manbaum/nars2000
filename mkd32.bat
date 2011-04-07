10aff445092ca7890e077f67e59e0c1671fd0d28
bsmith <bsmith@5d1e9092-042a-4af3-a4b6-76de6f5dccff>
2011-04-07 19:21:15 +0000

    Build #835 -- Version 0.0.2.23 ------------------------------
    * Reorganize Make Functions
    
    Directory trunk:
    
    MAKEFILE.INC:
      Move common definitions from <MAKEFILE.W32/.W64> to <MAKEFILE.INC>.
    
    MAKEFILE.IND:
      Define intermediate makefile to handle <DEP.TXT>.
    
    MAKEFILE.W32:
    MAKEFILE.W64:
      Update copyright year.
      Move common definitions from <MAKEFILE.W32/.W64> to <MAKEFILE.INC>.
    
    MK32.BAT:
    MK64.BAT:
    MKD32.BAT:
    MKD64.BAT:
    MKN.BAT:
      Test for "/n" and "DEL" in command line so we can handle them separately.

--- mkd32.bat
+++ mkd32.bat
@@ -2,5 +2,30 @@
 Set NMAKE=NMAKE
 if "%windir%" == "" Set NMAKE=NMAKER
 call w32
-%NMAKE% /nologo /f makefile.w32 DEBUG=1 %1 %2 %3 %4 %5 %6 %7 %8 %9
+
+Set NMAKE_SWS=
+if /i "%1"=="/n" Set NMAKE_SWS=/n %NMAKE_SWS%
+if /i "%2"=="/n" Set NMAKE_SWS=/n %NMAKE_SWS%
+if /i "%3"=="/n" Set NMAKE_SWS=/n %NMAKE_SWS%
+if /i "%4"=="/n" Set NMAKE_SWS=/n %NMAKE_SWS%
+if /i "%5"=="/n" Set NMAKE_SWS=/n %NMAKE_SWS%
+if /i "%6"=="/n" Set NMAKE_SWS=/n %NMAKE_SWS%
+if /i "%7"=="/n" Set NMAKE_SWS=/n %NMAKE_SWS%
+if /i "%8"=="/n" Set NMAKE_SWS=/n %NMAKE_SWS%
+if /i "%9"=="/n" Set NMAKE_SWS=/n %NMAKE_SWS%
+
+Set DEL_SW=
+if /i "%1"=="del" Set DEL_SW=1
+if /i "%2"=="del" Set DEL_SW=1
+if /i "%3"=="del" Set DEL_SW=1
+if /i "%4"=="del" Set DEL_SW=1
+if /i "%5"=="del" Set DEL_SW=1
+if /i "%6"=="del" Set DEL_SW=1
+if /i "%7"=="del" Set DEL_SW=1
+if /i "%8"=="del" Set DEL_SW=1
+if /i "%9"=="del" Set DEL_SW=1
+
+%NMAKE% /nologo /f makefile.ind "WIN=32" "DEBUG=1" "NMAKE_SWS=%NMAKE_SWS%" "DEL_SW=%DEL_SW%" %*
+Set DEL_SW=
+Set NMAKE_SWS=
 Set NMAKE=
--- mkn.bat
+++ mkn.bat
@@ -1,7 +1,7 @@
 @echo off
 Set NMAKE=NMAKE
 if "%windir%" == "" Set NMAKE=NMAKER
-call w64
+call w32
 
 Set NMAKE_SWS=
 if /i "%1"=="/n" Set NMAKE_SWS=/n %NMAKE_SWS%
@@ -25,7 +25,7 @@ if /i "%7"=="del" Set DEL_SW=1
 if /i "%8"=="del" Set DEL_SW=1
 if /i "%9"=="del" Set DEL_SW=1
 
-%NMAKE% /nologo /f makefile.ind "WIN=64" "DEBUG=1" "NMAKE_SWS=%NMAKE_SWS%" "DEL_SW=%DEL_SW%" %*
+%NMAKE% /nologo /f makefile.ind "WIN=32" "DEBUG=0" "NMAKE_SWS=%NMAKE_SWS%" "DEL_SW=%DEL_SW%" %*
 Set DEL_SW=
 Set NMAKE_SWS=
 Set NMAKE=

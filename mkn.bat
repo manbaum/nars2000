d459d5cb35bb8d42733e0810e7876639480daf75
bsmith <bsmith@5d1e9092-042a-4af3-a4b6-76de6f5dccff>
2011-04-08 16:53:10 +0000

    Build #837 -- Version 0.0.2.23 ------------------------------
    * Reorganize Make Functions, Part 2
    
    Directory trunk:
    
    MAKEFILE.INC:
      Include empty DEL target so as to avoid error message.
      Check for existance of files first for DELPRO and DELDEP targets so as to avoid error message.
    
    MAKEFILE.IND:
      Omit DEL target as it's no longer needed.
    
    MK32.BAT:
    MKD32.BAT:
    MK64.BAT:
    MKD64.BAT:
      Omit tests for switches as the whole command line is passed to <makefile.ind>.

--- mkn.bat
+++ mkn.bat
@@ -3,17 +3,6 @@ Set NMAKE=NMAKE
 if "%windir%" == "" Set NMAKE=NMAKER
 call w32
 
-Set NMAKE_SWS=
-if /i "%1"=="/n" Set NMAKE_SWS=/n %NMAKE_SWS%
-if /i "%2"=="/n" Set NMAKE_SWS=/n %NMAKE_SWS%
-if /i "%3"=="/n" Set NMAKE_SWS=/n %NMAKE_SWS%
-if /i "%4"=="/n" Set NMAKE_SWS=/n %NMAKE_SWS%
-if /i "%5"=="/n" Set NMAKE_SWS=/n %NMAKE_SWS%
-if /i "%6"=="/n" Set NMAKE_SWS=/n %NMAKE_SWS%
-if /i "%7"=="/n" Set NMAKE_SWS=/n %NMAKE_SWS%
-if /i "%8"=="/n" Set NMAKE_SWS=/n %NMAKE_SWS%
-if /i "%9"=="/n" Set NMAKE_SWS=/n %NMAKE_SWS%
-
 Set DEL_SW=
 if /i "%1"=="del" Set DEL_SW=1
 if /i "%2"=="del" Set DEL_SW=1
@@ -25,7 +14,6 @@ if /i "%7"=="del" Set DEL_SW=1
 if /i "%8"=="del" Set DEL_SW=1
 if /i "%9"=="del" Set DEL_SW=1
 
-%NMAKE% /nologo /f makefile.ind "WIN=32" "DEBUG=0" "NMAKE_SWS=%NMAKE_SWS%" "DEL_SW=%DEL_SW%" %*
+%NMAKE% /nologo /f makefile.ind "WIN=32" "DEBUG=0" "NMAKE_SWS=%*" "DEL_SW=%DEL_SW%"
 Set DEL_SW=
-Set NMAKE_SWS=
 Set NMAKE=

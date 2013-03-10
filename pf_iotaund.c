4b12c0e7263732403d23906cad8b77fc75da4c9d
sudleyplace <sudleyplace@gmail.com>
2013-03-10 21:27:55 +0000

    Build #1055 -- Version 0.3.8.xx -------------------------------
    * Fix Bug:  IotaUnderbar On Multirank APAs Crashes
    
    Directory trunk:
    
    PF_IOTAUND.C:
      Update copyright year.
      For monadic IotaUnderbar, if the arg is a multirank APA, calculate the next repetition value appropriately.

--- pf_iotaund.c
+++ pf_iotaund.c
@@ -4,7 +4,7 @@
 
 /***************************************************************************
     NARS2000 -- An Experimental APL Interpreter
-    Copyright (C) 2006-2012 Sudley Place Software
+    Copyright (C) 2006-2013 Sudley Place Software
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -546,8 +546,8 @@ LPPL_YYSTYPE PrimFnMonIotaUnderbar_EM_YY
             if (CheckCtrlBreak (*lpbCtrlBreak))
                 goto ERROR_EXIT;
 
-            // Get and skip over the repetition value
-            uLen = *lpMemRep++;
+            // Get the next repetition value
+            uLen = (IsSimpleAPA (aplTypeRht)) ? apaOff + apaMul * uRht : *lpMemRep++;
 
             // If it's non-zero (actually positive, but negative numbers have been ruled out above), ...
             if (uLen

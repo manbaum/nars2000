f721ef41bb69eba4c09c0e8753fe0b8054dec368
sudleyplace <sudleyplace@gmail.com>
2013-03-13 22:35:57 +0000

    Build #1061 -- Version 0.3.9.xx -------------------------------
    * Miscellaneous Build File Changes
    
    Directory trunk:
    
    DEPINIT.BAT:
      Avoid calling php as that only slows down the process.
    
    DEPTXT.BAT:
      Avoid calling php as that only slows down the process, and on a fast system may trigger an error message saying that "The process cannot access the file because it is being used by another process.".
    
    ECHODOT.TXT:
      A one-byte file containing a dot ".".  No trailing CR/LF is present so that typing the file from multiple calls spits out a series of dots all on one line until it wraps at the right edge of the CMD window.
    
    VER.RC:
      Increment version # to 0.3.9.xx.

--- depinit.bat
+++ depinit.bat
@@ -1,5 +1,5 @@
 @echo off
-echodot
+type echodot.txt
 if not exist (%1) exit /b
 echo ### > %1
 touch %1 /t000000 > NUL
--- deptxt.bat
+++ deptxt.bat
@@ -2,4 +2,3 @@
 echo !if exist ($(O)%1)   >> %2
 echo !    include $(O)%1  >> %2
 echo !endif               >> %2
-echodot

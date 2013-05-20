292ef09354f8f94b6403a7c416638c6714fd9326
sudleyplace <sudleyplace@gmail.com>
2013-05-20 16:51:08 +0000

    Build #1094 -- Version 0.3.10.xx --------------------------------
    * Simplify []WA Algorithm
    
    Directory trunk:
    
    QF_WA.C:
      Update copyright year.
      Use <GlobalMemoryStatusEx> to get available memory instead of allocating it as the latter might cause the page file to grow.

--- qf_wa.c
+++ qf_wa.c
@@ -4,7 +4,7 @@
 
 /***************************************************************************
     NARS2000 -- An Experimental APL Interpreter
-    Copyright (C) 2006-2011 Sudley Place Software
+    Copyright (C) 2006-2013 Sudley Place Software
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -45,10 +45,7 @@ LPPL_YYSTYPE SysFnWA_EM_YY
 
 {
     LPPL_YYSTYPE   lpYYRes;         // Ptr to the result
-    APLU3264       ByteTmpHi,       // # bytes to allocate, high value
-                   ByteTmpMid,      // ...                  mid  ...
-                   ByteTmpLo;       // ...                  low  ...
-    HGLOBAL        hGlbTmp;         // Temporary global memory handle
+    MEMORYSTATUSEX msex;            // Memory Status struc
 
     // This function is niladic
     Assert (lptkLftArg EQ NULL && lptkRhtArg EQ NULL);
@@ -60,51 +57,8 @@ LPPL_YYSTYPE SysFnWA_EM_YY
     if (lptkAxis NE NULL)
         goto AXIS_SYNTAX_EXIT;
 
-#ifdef _WIN64
-    // Start with 256GB-1
-    ByteTmpHi  = 0x3FFFFFFFFF;
-
-    // The amount of storage we don't care about
-  #define FUDGE_FACTOR    (128*1024)
-
-#elif defined (_WIN32)
-    // Start with 4GB-1
-    ByteTmpHi  = 0xFFFFFFFF;
-
-    // The amount of storage we don't care about
-  #define FUDGE_FACTOR    (16*1024)
-
-#else
-  #error Need code for this architecture.
-#endif
-    ByteTmpMid = ByteTmpHi / 2;
-    ByteTmpLo  = 1;
-
-    while (ByteTmpHi > (ByteTmpLo + FUDGE_FACTOR))
-    {
-        // Coalesce memory by allocating a large contiguous block
-        // We use GlobalAlloc/Free instead of MyGlobalAlloc/Free to
-        //   avoid debug messages upon failure.
-        hGlbTmp = GlobalAlloc (GMEM_MOVEABLE, (APLU3264) ByteTmpMid);
-        if (hGlbTmp)
-        {
-            // We no longer need this storage
-            GlobalFree (hGlbTmp); hGlbTmp = NULL;
-
-            // Save as low water mark
-            ByteTmpLo = ByteTmpMid;
-
-            // Split the difference between Hi and Mid
-            ByteTmpMid = (APLU3264) ((APLUINT) ByteTmpHi + (APLUINT) ByteTmpMid) / 2;
-        } else
-        {
-            // Save as high water mark
-            ByteTmpHi = ByteTmpMid;
-
-            // Split the difference between Lo and Mid
-            ByteTmpMid = (APLU3264) ((APLUINT) ByteTmpLo + (APLUINT) ByteTmpMid) / 2;
-        } // End IF/ELSE
-    } // End WHILE
+    msex.dwLength = sizeof (msex);      // Initialize the memory status struc
+    GlobalMemoryStatusEx (&msex);       // Get the global memory status
 
     // Allocate a new YYRes
     lpYYRes = YYAlloc ();
@@ -113,7 +67,7 @@ LPPL_YYSTYPE SysFnWA_EM_YY
     lpYYRes->tkToken.tkFlags.TknType   = TKT_VARIMMED;
     lpYYRes->tkToken.tkFlags.ImmType   = IMMTYPE_INT;
 ////lpYYRes->tkToken.tkFlags.NoDisplay = FALSE;         // Already zero from YYAlloc
-    lpYYRes->tkToken.tkData.tkInteger  = ByteTmpLo;
+    lpYYRes->tkToken.tkData.tkInteger  = msex.ullAvailPhys + msex.ullAvailPageFile;
     lpYYRes->tkToken.tkCharIndex       = lptkFunc->tkCharIndex;
 
     return lpYYRes;

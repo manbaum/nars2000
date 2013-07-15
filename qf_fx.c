fe9b5895ad3712a3f6bdc89d774c7d123e7ea32b
sudleyplace <sudleyplace@gmail.com>
2013-07-15 04:03:36 +0000

    Build #1128 -- Version 0.4.0.xx -------------------------------
    * Fix Bug In []FX
    
    Directory trunk:
    
    QF_FX.C:
      Update copyright year.
      Fix bug in <SysFnMonFX_EM_YY> when fixing a function from a nested vector with a scalar item.

--- qf_fx.c
+++ qf_fx.c
@@ -4,7 +4,7 @@
 
 /***************************************************************************
     NARS2000 -- An Experimental APL Interpreter
-    Copyright (C) 2006-2011 Sudley Place Software
+    Copyright (C) 2006-2013 Sudley Place Software
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -200,7 +200,7 @@ LPPL_YYSTYPE SysFnMonFX_EM_YY
                             goto RIGHT_DOMAIN_EXIT;
 
                         // Ensure the item is simple char
-                        if (!IsSimpleChar (aplTypeItmRht))
+                        if (!IsImmChr (immTypeItmRht))
                             goto RIGHT_DOMAIN_EXIT;
                     } // End IF/ELSE
                 } // End FOR

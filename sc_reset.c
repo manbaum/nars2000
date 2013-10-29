5e13ce8a94e0d15c904cb1bc18e2e4b331111fd0
sudleyplace <sudleyplace@gmail.com>
2013-10-29 14:35:16 +0000

    Build #1167 -- Version 0.4.4.xx -------------------------------
    * Display Prompt After )RESET With No Suspension
    
    Directory trunk:
    
    SC_RESET.C:
      Update copyright year.
      As it turns out, when executing )RESET with no suspension, at no point is the six-space indent displayed, so we explicitly display the prompt.

--- sc_reset.c
+++ sc_reset.c
@@ -4,7 +4,7 @@
 
 /***************************************************************************
     NARS2000 -- An Experimental APL Interpreter
-    Copyright (C) 2006-2010 Sudley Place Software
+    Copyright (C) 2006-2013 Sudley Place Software
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -57,7 +57,8 @@ UBOOL CmdReset_EM
 
         // Signal the next layer to begin resetting
         MyReleaseSemaphore (lpMemPTD->lpSISCur->hSemaphore, 1, NULL);
-    } // End IF
+    } else
+        DisplayPrompt (GetThreadSMEC (), 3);
 
     return TRUE;
 } // End CmdReset_EM

3ce97beba28e0f1216c7974de5c2e2a3b1077df6
sudleyplace <sudleyplace@gmail.com>
2014-03-12 22:05:41 +0000

    Build #1230 -- Version 0.4.6.xx -------------------------------
    * Fix Bug When Displaying An Exec/Quad Token With No Value
    
    Directory trunk:
    
    PL_PROC.C:
      Update copyright year.
      In <ArrExprCheckCaller>, when an Exec/Quad result token is present, ensure it has a value before trying to display it.

--- pl_proc.c
+++ pl_proc.c
@@ -4,7 +4,7 @@
 
 /***************************************************************************
     NARS2000 -- An Experimental APL Interpreter
-    Copyright (C) 2006-2013 Sudley Place Software
+    Copyright (C) 2006-2014 Sudley Place Software
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -1245,8 +1245,11 @@ void ArrExprCheckCaller
     // Set/clear the NoDisplay flag
     lpplLocalVars->lpYYRes->tkToken.tkFlags.NoDisplay = bNoDisplay;
 
-    // If the Execute/Quad result is present, display it
-    if (lpMemPTD->YYResExec.tkToken.tkFlags.TknType)
+    // If the Execute/Quad result is present,
+    //   and the token is not named or it has a value, ...
+    if (lpMemPTD->YYResExec.tkToken.tkFlags.TknType
+     && (!IsTknNamed (&lpMemPTD->YYResExec.tkToken)
+      || !IsSymNoValue (lpMemPTD->YYResExec.tkToken.tkData.tkSym)))
     {
         HGLOBAL hGlbDfnHdr;
 

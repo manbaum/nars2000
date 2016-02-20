dfb1f7e2edb8d289b238badb6bbd0e1dab046bc2
sudleyplace <sudleyplace@5d1e9092-042a-4af3-a4b6-76de6f5dccff>
2016-02-20 18:55:55 +0000

    Build #1449 -- Version 0.4.13.xx --------------------------------
    * Minor Build, DEBUG, And Documentation Changes
    
    Directory trunk:
    
    MAKEFILE.INC:
      Include previous CFLAGS in /GL line.
    
    VER.RC:
      Increment version # to 0.4.13.xx.
    
    
    Directory trunk\ReleaseNotes:
    
    NOTES-1425.TXT:
    NOTES-1430.TXT:
    NOTES-1431.TXT:
    NOTES-1432.TXT:
    NOTES-1433.TXT:
    NOTES-1434.TXT:
    NOTES-1435.TXT:
    NOTES-1436.TXT:
    NOTES-1437.TXT:
    NOTES-1438.TXT:
    NOTES-1439.TXT:
    VERSION-0.4.12.XX.TXT:
      Minor editing and format changes.
    
    
    Directory trunk\htdocs:
    
    FOOTER-MON.HTML:
    FOOTER.HTML:
    INDEX-BODY.AHTML:
      Update copyright year.
    
    
    Directory trunk\htdocs\download:
    
    DOWNLOAD-BODY.PHP:
      Include .pdb files in the list of files available for download..
      Fix typo in id="Beta_Versions" (Missing underbar).
    
    DOWNLOAD-LEFT.HTML:
      Fix typo in id="Beta_Versions" (Missing underbar).

--- htdocs/download/Download-body.php
+++ htdocs/download/Download-body.php
@@ -52,11 +52,13 @@ Downloads</h1>
 
   <p>There are several types of files available for downloading:
     <b>NARS2000-<i>version</i>.exe</b> (or the earlier format files
-    <b>NARS2000-<i>version</i>.zip</b>), <b>NARS2000-<i>version</i>.map</b>,
+    <b>NARS2000-<i>version</i>.zip</b>),
+    <b>NARS2000-<i>version</i>.map</b>,
+    <b>NARS2000-<i>version</i>.pdb</b>,
     <b>Release Notes</b>, and <b>Special Files</b>.  The <b>.exe</b> (or earlier
     format <b>.zip</b>) files contain everything you
     need to run the program and are the recommended downloads for end
-    users.  The <b>.map</b> files contain debugging information and are used
+    users.  The <b>.map</b> and <b>.pdb</b> files contain debugging information and are used
     by developers.  The <b>Release Notes</b> contain information about the
     corresponding release and are, generally, of most use to developers.  The
     <b>Special Files</b> such as
@@ -173,7 +175,7 @@ Downloads</h1>
 </div>
 
 <div class="section">
-  <h2 id="Beta Versions">Beta Versions</h2>
+  <h2 id="Beta_Versions">Beta Versions</h2>
 
   <p>For the latest (<b>untested</b>) versions, goto the <a class="linkleft" href="binaries/beta">beta</a>
     directory and download either the 32- or 64-bit file.  This is a .zip file (as opposed to the released
@@ -230,9 +232,18 @@ Downloads</h1>
                      || file_exists ($DirName . "w64/" . $FileName . "exe"))
                         $Files[] = $FileName . "exe";
                     else
-////////////////////if (file_exists ($DirName . "w32/" . $FileName . "zip"))
+                    if (file_exists ($DirName . "w32/" . $FileName . "zip"))
                         $Files[] = $FileName . "zip";
-                    $Files[] = $FileName . "map";
+
+                    // Append the appropriate file (.map) to the list, if it exists
+                    if (file_exists ($DirName . "w32/" . $FileName . "map")
+                     || file_exists ($DirName . "w64/" . $FileName . "map"))
+                        $Files[] = $FileName . "map";
+
+                    // Append the appropriate file (.pdb) to the list, if it exists
+                    if (file_exists ($DirName . "w32/" . $FileName . "pdb")
+                     || file_exists ($DirName . "w64/" . $FileName . "pdb"))
+                        $Files[] = $FileName . "pdb";
                 } else
                     $Files[] = $File;
             } // End IF
@@ -322,7 +333,8 @@ Downloads</h1>
         var tr = rows[i];
 ////////var attr = tr.getAttribute ('class');   // Doesn't work in IE
         var attr = tr.attributes['class'];
-            if (attr && attr.value == 'map')
+            if (attr && (attr.value == 'map'
+                      || attr.value == 'pdb'))
                 tr.style.display = gAllFiles ? gTableRow : 'none';
         } // End FOR
 

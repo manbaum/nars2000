ed7cab032b78a17818863e76f87edc8b31530a32
sudleyplace <sudleyplace@5d1e9092-042a-4af3-a4b6-76de6f5dccff>
2017-02-27 15:28:53 +0000

    Build #1686 -- Version 0.5.1.xx --------------------------------
    * Fix Bug In []TRACE Display
    
    Directory branches/bsmith/NewTypes:
    
    QF_STRACE.C:
      Fix bug in <DisplayTraceResult> where it included an extra trailing zero in the NELM.

--- NewTypes/qf_strace.c
+++ NewTypes/qf_strace.c
@@ -4,7 +4,7 @@
 
 /***************************************************************************
     NARS2000 -- An Experimental APL Interpreter
-    Copyright (C) 2006-2016 Sudley Place Software
+    Copyright (C) 2006-2017 Sudley Place Software
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -845,8 +845,8 @@ UBOOL DisplayTraceResult
 
     CHECK_TEMP_OPEN
 
-    // Calculate the item NELM ("+ 1" for the trailing zero
-    aplNELMItm = lstrlenW (lpwszFcnLine) + 1;
+    // Calculate the item NELM
+    aplNELMItm = lstrlenW (lpwszFcnLine);
 
     // Calculate the result NELM
     aplNELMRes = 2;
